20403af5a892c09b4b2685970b8496c1e65bbdbd
Luis Fonseca <luispedrofonseca@gmail.com>
2016-07-02 19:26:51 +0800

    Script to set the framerate

---
+++ Assets/lpf-unity-tools/SetFrameRate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SetFrameRate : MonoBehaviour 
+{
+    public int FrameRate = 60;
+
+    void Awake()
+    {
+        Application.targetFrameRate = FrameRate;
+    }
+}
