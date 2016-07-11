9e0c3461a2fd9b3a8e4c0938a1ef7a8a1c8a328f
Luis Fonseca <luispedrofonseca@gmail.com>
2016-07-11 22:59:15 +0800

    Simple rotate object component

---
+++ Assets/lpf-unity-tools/RotateObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour
+{
+    public Vector3 Speed = Vector3.one;
+
+    Transform _transform;
+
+    void Awake()
+    {
+        _transform = transform;
+    }
+
+    void Update()
+    {
+        _transform.Rotate(Speed * Time.smoothDeltaTime);
+    }
+}
