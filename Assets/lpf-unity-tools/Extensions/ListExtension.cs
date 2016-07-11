b7e446c35bd7308a6ebbf42d7b937fa05253f571
Luis Fonseca <luispedrofonseca@gmail.com>
2016-07-11 15:58:15 +0800

    Method to add and return an item from a list

---
+++ Assets/lpf-unity-tools/Extensions/ListExtension.cs
@@ -0,0 +1,12 @@
+﻿// https://twitter.com/AngryAnt/status/751721482531774464
+
+using System.Collections.Generic;
+
+public static class ListExtension
+{
+    public static T AddAndReturn<T>(this List<T> list, T item)
+    {
+        list.Add(item);
+        return item;
+    }
+}
