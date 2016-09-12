addcc59d29bb0732eded07f4f9b7aa4a6133efa3
Luis Fonseca <luispedrofonseca@gmail.com>
2016-09-12 17:51:16 +0100

    Tool to clean the AssetBundles caches

---
+++ Assets/lpf-unity-tools/Editor/CleanAssetBundlesCache.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// An editor script which deletes cache info.
+///
+/// NOTE:
+///     Delete all AssetBundle content that has been cached by the current application.
+///     This function is not available to WebPlayer applications that use the shared cache.
+///
+/// See Also:
+///     http://docs.unity3d.com/Documentation/ScriptReference/Caching.CleanCache.html
+///
+/// </summary>
+/// 
+public class CleanAssetBundlesCache
+{
+    [MenuItem ("Tools/Cache/Clean AssetBundle caches")]
+    public static void CleanCache ()
+    {
+        if (Caching.CleanCache ()) 
+        {
+            Debug.LogWarning ("Successfully cleaned all AssetBundle caches.");
+        }
+        else 
+        {
+            Debug.LogWarning ("Cache was in use.");
+        }
+    }
+}
